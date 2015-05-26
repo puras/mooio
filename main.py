40d7edee065a30f03379b2c0299d163e3b183ed3
puras <puras.he@gmail.com>
2015-05-26 08:35:34 +0800

    去掉注释

--- main.py
+++ main.py
@@ -6,21 +6,22 @@ from pyquery import PyQuery as pq
 
 import urllib
 import os
-import logging
-
-log = logging.getLogger('mooio')
-hdlr = logging.FileHandler(os.path.join('mooio.log'))
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-hdlr.setFormatter(formatter)
-log.addHandler(hdlr)
-log.setLevel(logging.INFO)
+# import logging
+#
+# log = logging.getLogger('mooio')
+# hdlr = logging.FileHandler(os.path.join('mooio.log'))
+# formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+# hdlr.setFormatter(formatter)
+# log.addHandler(hdlr)
+# log.setLevel(logging.INFO)
 
 def do_download(root_url):
-    print u'开始加载页面'
-    log.info(u'开始加载页面:%s' % root_url)
+    print '开始加载页面:%s' % root_url
+    # log.info(u'开始加载页面:%s' % root_url)
     d = pq(url=root_url)
     posts = d('div.m-post')
-    log.info(u'本页共有 %s 组图片' % len(posts))
+    print '本页共有 %s 组图片' % len(posts)
+    # log.info(u'本页共有 %s 组图片' % len(posts))
     for post in posts:
         dd = pq(post)
         img = dd('img')
@@ -37,18 +38,21 @@ def do_download(root_url):
         pic_file = '%s/thumbnails_%s' % (folder, thumbnails)
         if not os.path.exists(pic_file):
             urllib.urlretrieve(pic, pic_file)
-        print '开始加载子页面'
-        log.info(u'正在加载: %s' % name)
-        log.info(u'开始加载子页面:%s' % picurl)
-        log.info(u'存入文件夹: %s' % folder)
+
+        print '正在加载: %s' % name
+        print '开始加载子页面:%s' % picurl
+        print '存入文件夹: %s' % folder
+        # log.info(u'正在加载: %s' % name)
+        # log.info(u'开始加载子页面:%s' % picurl)
+        # log.info(u'存入文件夹: %s' % folder)
         sd = pq(url=picurl)
         pics = sd('div.pic')
         num_file = '%s/pic_num.txt' % folder
         nf = open(num_file, 'w')
         nf.write('共有 %s 张图' % len(pics))
         nf.close()
-        print u'共有 %s 张图' % len(pics)
-        log.info(u'共有 %s 张图' % len(pics))
+        print '共有 %s 张图' % len(pics)
+        # log.info(u'共有 %s 张图' % len(pics))
         for spic in pics:
             sdd = pq(spic)
             sexy_pic = sdd('img').attr('src')
@@ -57,10 +61,12 @@ def do_download(root_url):
             print sexy_name
             sexy_file = '%s/%s' % (folder, sexy_name)
             if not os.path.exists(sexy_file):
-                log.info(u'正在下载：%s' % sexy_pic)
+                print '正在下载：%s' % sexy_pic
+                # log.info(u'正在下载：%s' % sexy_pic)
                 urllib.urlretrieve(sexy_pic, sexy_file)
             else:
-                log.info(u'本图片已下载: %s' % sexy_pic)
+                print '本图片已下载: %s' % sexy_pic
+                # log.info(u'本图片已下载: %s' % sexy_pic)
             # break
 
         # break
@@ -71,8 +77,8 @@ if __name__ == '__main__':
     for i in range(1, 40):
         print i
         download_url = '%s%d' % (root_url, i)
-        print download_url
-        log.info(u'当前下载地址: %s' % download_url)
+        print '当前下载地址: %s' % download_url
+        # log.info(u'当前下载地址: %s' % download_url)
 
         do_download(download_url)
 
